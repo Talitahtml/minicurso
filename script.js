18e41e537158950cd0ed7db5568a65e82c9a0f8e
elica <elica.vaz@gmail.com>
2022-01-15 23:39:34 -0300

    Corrigir html

--- script.js
+++ script.js
@@ -1,24 +1,23 @@
 const imagem = document.querySelector('img');
-const botao = document.querySelector ('button');
+const botao = document.querySelector('button');
 const nomeDoPersonagem = document.querySelector('#nome');
 const especie = document.querySelector('#especie');
-const condicao = document.querySelector('#status')
+const condicao = document.querySelector('#status');
 
 pegarPersonagem = () => {
-     return fetch(`https://rickandmortyapi.com/api/character/2` {
-         method:'GET',
-         header: {
-             Accept: 'application/json',
-             "Content-type": 'application/json'
-         }
-     }).then((response) => response.json()).then((data) => {
-         imagem.src = data.image;
-         imagem.alt = data.name;
-         nomeDoPersonagem.innerHTML = data.name
-        })
+   return fetch(`https://rickandmortyapi.com/api/character/2`, {
+       method: 'GET',
+       headers: {
+           Accept: "application/json",
+           "Content-type": "application/json"
+       }
+   }).then((response) => response.json()).then((data) => {
+       imagem.src = data.image;
+       imagem.alt = data.name;
+       nomeDoPersonagem.innerHTML = data.name
+       especie.innerHTML = data.especies;
+       condicao.innerHTML = data.status
+   })
 }
 
 botao.onclick = pegarPersonagem
-
-especie.innerHTML = data.especies
-condicao.innerHTML = data.status
