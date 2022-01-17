cb91ff6fd4f095391231094e904184f21ad4089d
elica <elica.vaz@gmail.com>
2022-01-16 22:26:34 -0300

    Implementar as três imagens na tela

--- script.js
+++ script.js
@@ -1,29 +1,49 @@
-const imagem = document.querySelector('img');
+let indice
+let imagem = []
+let nomeDoPersonagem = []
+let especie = []
+let condicao = []
+
+for (let i=0; i<3; i++){
+    let x = document.getElementById('img'+[i]);
+    let n = document.getElementById('nome'+[i]);
+    let e = document.getElementById('especie'+[i]);
+    let c = document.getElementById('status'+[i]);
+    imagem.push(x)
+    nomeDoPersonagem.push(n);
+    especie.push(e);
+    condicao.push(c);
+}
+
 const botao = document.querySelector('button');
-const nomeDoPersonagem = document.querySelector('#nome');
-const especie = document.querySelector('#especie');
-const condicao = document.querySelector('#status');
+
 
 gerarValorAleatorio = () => {
     return Math.floor(Math.random() * 671)
 }
 
 pegarPersonagem = () => {
-    let numeroAleatorio = gerarValorAleatorio();
-   return fetch(`https://rickandmortyapi.com/api/character/${numeroAleatorio}`, {
-       method: 'GET',
-       headers: {
-           Accept: "application/json",
-           "Content-type": "application/json"
-       }
-   }).then((response) => response.json()).then((data) => {
-       imagem.src = data.image;
-       imagem.alt = data.name;
-       nomeDoPersonagem.innerHTML = data.name
-       especie.innerHTML = data.species;
-       condicao.innerHTML = data.status
-   })
+    for (let i=0; i<3; i++){
+        let numeroAleatorio = gerarValorAleatorio();
+        t =  fetch(`https://rickandmortyapi.com/api/character/${numeroAleatorio}`, {
+            method: 'GET',
+            headers: {
+                Accept: "application/json",
+                "Content-type": "application/json"
+             }
+            }).then((response) => response.json()).then((data) => {
+                imagem[i].src = data.image;
+                imagem[i].alt = data.name;
+                nomeDoPersonagem[i].innerHTML = data.name;
+                especie[i].innerHTML = data.species;
+                condicao[i].innerHTML = data.status;
+            })
+    }
+    return t
 }
 
+
+
 botao.onclick = pegarPersonagem
+ 
 
